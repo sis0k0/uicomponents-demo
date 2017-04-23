1ee1d40dbecdb263021ee1bea7adb23b6018e443
sis0k0 <stanimira.vlaeva@gmail.com>
2017-04-23 18:59:00 +0300

    feat: inject NinjasService in AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { NinjasService } from "./ninjas.service";
+import { Ninja } from "./ninja.model";
 
 @Component({
     selector: "ns-app",
     templateUrl: "./app.component.html",
 })
-export class AppComponent { }
+export class AppComponent {
+    ninjas: Ninja[];
+
+    constructor(private ninjaService: NinjasService) {
+    }
+
+    ngOnInit() {
+        this.ninjas = this.ninjaService.getItems();
+    }
+}
